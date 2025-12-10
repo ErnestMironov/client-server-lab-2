32e4d549e1851659806c26a8a59fedc0250bd521
ermirgurt <erik1297e@gmail.com>
2025-12-10 04:35:46 +0300

    Step 4: Implement StudentController with createStudentDataInData method

--- data-service/src/main/java/ru/mironov/dataservice/controller/StudentController.java
+++ data-service/src/main/java/ru/mironov/dataservice/controller/StudentController.java
@@ -1,4 +1,39 @@
 package ru.mironov.dataservice.controller;
 
-public class StudentController {
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import ru.mironov.data.api.StudentDataApi;
+import ru.mironov.data.model.StudentDataCreateRequest;
+import ru.mironov.data.model.StudentDataResponse;
+import ru.mironov.dataservice.data.Student;
+import ru.mironov.dataservice.data.StudentRepository;
+
+@RestController
+@RequiredArgsConstructor
+public class StudentController implements StudentDataApi {
+
+    private final StudentRepository studentRepository;
+
+    @Override
+    public ResponseEntity<StudentDataResponse> createStudentDataInData(StudentDataCreateRequest request) {
+        Student student = new Student();
+        student.setName(request.getFullName());
+        student.setPassport(request.getPassport());
+        studentRepository.save(student);
+
+        StudentDataResponse response = new StudentDataResponse();
+        response.setId(student.getId());
+        response.setFullName(student.getName());
+        response.setPassport(student.getPassport());
+
+        return ResponseEntity.status(200).body(response);
+    }
+
+    /*@Override
+    public ResponseEntity<StudentDataResponse> getStudentDataByIdFromData(Long id) {
+
+        return ResponseEntity.status(200).body(response);
+    }*/
 }
